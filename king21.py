1235d5facf66daf2f2066142b4fcd53f30a346af
kingwangz <540886982@qq.com>
2017-07-13 16:21:01 +0800

    king2

--- king21.py
+++ king21.py
@@ -1,6 +1,21 @@
-import jieba
-nu='''d
-   d'''
-print(nu)
-print(nu)
-print(nu)
+class FooClass(object):
+    """my very first class: FooClass"""
+    version = 0.1
+
+    def __init__(self, nm='John Doe'):
+        """constructor"""
+        self.name = nm
+
+    def showname(self):
+        """display instance attribute and class name"""
+        print('Your name is', self.name)
+        print('My name is', self.__class__.__name__)
+
+    def showver(self):
+        """display class(static) attribute"""
+        print(self.version)
+
+    def addMe2Me(self, x):
+        return x + x
+foo1 = FooClass()
+foo1.showver()
