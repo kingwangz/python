4277fc4a40d73a36890d2cc34d61f5dd4aac23ff
kingwangz <540886982@qq.com>
2017-07-14 11:21:00 +0800

    king2

---
+++ king22.py
@@ -0,0 +1,21 @@
+import os
+
+ls = os.linesep
+while True:
+    fname = input()
+    if os.path.exists(fname):
+        print("ERROR: '%s' already exists".format(fname))
+    else:
+        break
+all = []
+print("\nEnter lines ('.' by itself to quit).\n")
+while True:
+    entry = input('> ')
+    if entry == '.':
+        break
+    else:
+        all.append(entry)
+fobj = open(fname, 'w')
+fobj.writelines(['%s%s'.format(x, ls) for x in all])
+fobj.close()
+print('DONE!')
