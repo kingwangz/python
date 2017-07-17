d7110079568f221a03480a34568f47d181a20ce2
kingwangz <540886982@qq.com>
2017-07-17 14:44:33 +0800

    king24

---
+++ king24.py
@@ -0,0 +1,20 @@
+import string
+
+alphas = string.ascii_letters + '_'
+nums = string.digits
+inp = input('Identifier to test? ')
+if len(inp) == 1:
+    if inp[0] not in alphas:
+        print('invalid: first symbol must bealphabetic')
+    else:
+        print("okay as an identifier")
+elif len(inp) > 1:
+    if inp[0] not in alphas:
+        print('invalid: first symbol must bealphabetic')
+    else:
+        for otherChar in inp[1:]:
+            if otherChar not in alphas + nums:
+                print('invalid: remaining symbols must be alphanumeric')
+                break
+        else:
+            print("okay as an identifier")
