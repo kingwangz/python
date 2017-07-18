77b90ed5d1063e66816e42ae76336d5694812017
kingwangz <540886982@qq.com>
2017-07-18 18:41:32 +0800

    king26

--- king26.py
+++ king26.py
@@ -9,3 +9,9 @@ s = set('cheeseshop')
 print(s)
 t = frozenset('bookshop')
 print(t)
+myTuple = (123, 'xyz', 0, 45.67)
+legends = {('Poe', 'author'): (1809, 1849, 1976), ('Gaudi', 'architect'): (1852, 1906, 1987),
+           ('Freud', 'psychoanalyst'): (1856, 1939, 1990)}
+# i = iter(legends)
+for k in legends:
+    print(k)
