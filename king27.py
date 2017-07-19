c6dc29aa256a4aec53d7b1d8c13b9107b70ecbff
kingwangz <540886982@qq.com>
2017-07-19 15:00:11 +0800

    king27关联king21

---
+++ king27.py
@@ -0,0 +1,16 @@
+'''if __name__ == "__main__":
+
+    print("two.py is being run directly")
+else:
+    print("two.py is being imported into another module")
+
+
+def func():
+    print("func() in one.py")
+
+if __name__ == "__main__":
+    print("one.py is being run directly")
+else:
+    print("one.py is being imported into another module")
+'''
+import king21
