e09775fafbc3b9446e28aeef10b727c564852a9e
kingwangz <540886982@qq.com>
2017-07-20 18:18:41 +0800

    king32

--- king32.py
+++ king32.py
@@ -8,3 +8,29 @@ mathObj.x = 2
 mathObj.y = 3
 print(mathObj.x + mathObj.y)
 print(mathObj.x * mathObj.y)
+
+
+class MyDataWithMethod(object):
+    def printFoo(self):
+        print('You invoked printFoo()!')
+
+
+myObj = MyDataWithMethod()
+myObj.printFoo()
+
+
+class addrBookEntry(object):
+    def __init__(self, nm, ph):
+        self.phone = ph
+        self.name = nm
+        print('Created instance for:', self.name)
+        print('Created instance for:', self.phone)
+
+    def updatePhone(self, newph):
+        self.phone = newph
+        print('Updated phone# for:', self.name)
+
+
+john = addrBookEntry('John Doe', '408-555-1212')
+john.name
+john.phone
