9166790b6296a75fe08fb703efc40b6e9f4d7b9b
kingwangz <540886982@qq.com>
2017-07-26 12:20:21 +0800

    king35

---
+++ king35.py
@@ -0,0 +1,21 @@
+class P1:
+    print('called P1-foo()')
+
+
+class P2:
+    print('called P2-foo()')
+
+    def bar(self):
+        print('called P2-bar()')
+
+
+class C1(P1, P2):
+    pass
+
+
+class C2(P1, P2):
+    print('called C2-bar()')
+
+
+class GC(C1, C2):
+    pass
