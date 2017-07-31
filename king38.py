2b8a0ac00c32de86533a8a1e4a8ce7f2f6a146ea
kingwangz <540886982@qq.com>
2017-07-31 14:42:48 +0800

    king38

---
+++ king38.py
@@ -0,0 +1,15 @@
+class WrapMe(object):
+    def __init__(self, obj):
+        self.__data = obj
+
+    def get(self):
+        return self.__data
+
+    def __repr__(self):
+        return 'self.__data'
+
+    def __str__(self):
+        return str(self.__data)
+
+    def __getattr__(self, attr):
+        return getattr(self.__data, attr)
